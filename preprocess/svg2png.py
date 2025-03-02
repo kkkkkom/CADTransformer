c5000297e2f18135db78c220bf2551e2eaf351bf
kkkkkom <iamknew3@gmail.com>
2025-03-02 10:41:38 -0800

    update svg2png

--- preprocess/svg2png.py
+++ preprocess/svg2png.py
@@ -7,6 +7,8 @@ from glob import glob
 from multiprocessing import Pool
 from functools import partial
 from _utils_dataset import *
+import subprocess
+
 
 def cvt_svg2png(svg_path, replace1, replace2, scale=7):
     '''Function: convert svg to png
@@ -14,6 +16,7 @@ def cvt_svg2png(svg_path, replace1, replace2, scale=7):
     out_path = svg_path.replace(replace1, replace2)
     out_path = out_path.replace(".svg", ".png")
     svg2png(svg_path, out_path, scale=scale)
+
 
 def combine_trainset(dir0, dir1, dir_all):
     '''Function: Combine the two training subsets into one
@@ -29,6 +32,7 @@ def combine_trainset(dir0, dir1, dir_all):
             print(f"Duplicated:  > {save_path}")
         shutil.copy(f, save_path)
 
+
 def cvt_line_color(svg_path, output_dir=None):
     '''Convert line color into black to align with other floorplan dataset
     '''
@@ -41,7 +45,8 @@ def cvt_line_color(svg_path, output_dir=None):
     else:
         raise NotImplementedError
 
-def scaleSVG(svg_path, scale, cvt_color=False):
+
+def scaleSVG_bak(svg_path, scale, cvt_color=False):
     '''Scale the coordinates of segments
     '''
     out_path = svg_path
@@ -53,7 +58,7 @@ def scaleSVG(svg_path, scale, cvt_color=False):
         if tag == "svg":
             viewBox = line["viewBox"]
             viewBox = viewBox.split(" ")
-            viewBox = [str(float(v)*scale) for v in viewBox]
+            viewBox = [str(float(v) * scale) for v in viewBox]
             line["viewBox"] = " ".join(viewBox)
             if cvt_color:
                 line["style"] = "background-color: #255255255;"
@@ -65,27 +70,38 @@ def scaleSVG(svg_path, scale, cvt_color=False):
         if tag == "circle":
             cx, cy, rad = float(line["cx"]), float(line["cy"]), float(line["r"])
             line["cx"], line["cy"], line["r"] = \
-                     str(cx * scale), str(cy * scale), str(rad * scale)
+                str(cx * scale), str(cy * scale), str(rad * scale)
 
         if tag == "ellipse":
             cx, cy, rad_x, rad_y = float(line["cx"]), \
-                     float(line["cy"]), float(line["rx"]), float(line["ry"])
+                float(line["cy"]), float(line["rx"]), float(line["ry"])
             line["cx"], line["cy"], line["rx"], line["ry"] = \
-                     str(cx * scale), str(cy * scale), str(rad_x * scale), str(rad_y * scale)
-            #this is a bug in release v1
+                str(cx * scale), str(cy * scale), str(rad_x * scale), str(rad_y * scale)
+            # this is a bug in release v1
             transform = str(line["transform"])
             if "rotate" in transform:
                 transform = transform.split("rotate(")[1].split(")")[0]
                 transform = transform.split(",")
                 if len(transform) == 3:
                     transform = [float(tf) for tf in transform]
-                    transform = "rotate({},{},{})".format(str(transform[0]),str(cx * scale),str(cy * scale))
+                    transform = "rotate({},{},{})".format(str(transform[0]), str(cx * scale), str(cy * scale))
                     line["transform"] = transform
         if "stroke-width" in line.keys():
-            line["stroke-width"] = str(float(line["stroke-width"])*scale)
+            line["stroke-width"] = str(float(line["stroke-width"]) * scale)
 
     svg_writer(parsing_list, out_path)
     return out_path
+
+
+def scaleSVG(svg_path, scale, cvt_color=False):
+    '''Scale the coordinates of segments
+    '''
+    out_path = svg_path
+    subprocess.run(
+        f'inkscape - -actions = "select-all:all;transform-scale:{scale},{scale};fit-canvas-to-selection;export-filename:{out_path};export-do" {svg_path}',
+        shell=True)
+    return out_path
+
 
 def main():
     '''Main entrance'''
@@ -111,20 +127,20 @@ def main():
 
     random.seed(args.seed)
 
-    src = f"{args.data_save_dir}/train/train/svg_gt/*.svg"
-    dst = f"{svg_dir}/train"
-    cmd = f"cp -r {src}  {dst}"
-    os.system(cmd)
-
-    src = f"{args.data_save_dir}/val/val/svg_gt/*.svg"
-    dst = f"{svg_dir}/val"
-    cmd = f"cp -r {src}  {dst}"
-    os.system(cmd)
-
-    src = f"{args.data_save_dir}/test/test/svg_gt/*.svg"
-    dst = f"{svg_dir}/test"
-    cmd = f"cp -r {src}  {dst}"
-    os.system(cmd)
+    # src = f"{args.data_save_dir}/train/train/svg_gt/*.svg"
+    # dst = f"{svg_dir}/train"
+    # cmd = f"cp -r {src}  {dst}"
+    # os.system(cmd)
+    #
+    # src = f"{args.data_save_dir}/val/val/svg_gt/*.svg"
+    # dst = f"{svg_dir}/val"
+    # cmd = f"cp -r {src}  {dst}"
+    # os.system(cmd)
+    #
+    # src = f"{args.data_save_dir}/test/test/svg_gt/*.svg"
+    # dst = f"{svg_dir}/test"
+    # cmd = f"cp -r {src}  {dst}"
+    # os.system(cmd)
 
     # # # convert line color to black
     if args.cvt_color:
@@ -183,6 +199,7 @@ def main():
     # for svg_path in svg_paths:
     #     print("svg_path:", svg_path)
     #     scaleSVG(svg_path, args.svg_dir, scale=args.scale)
+
 
 if __name__ == '__main__':
     main()
